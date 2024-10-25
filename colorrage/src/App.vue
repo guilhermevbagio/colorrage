7bcba0f712cb821d854f1b838c9ea346fce07eaf
Guilherme <guilhermevbagio@gmail.com>
2024-10-25 09:35:12 -0300

    .

--- colorrage/src/App.vue
+++ colorrage/src/App.vue
@@ -1,21 +1,61 @@
-<script setup>
-import ColorSquare from "./components/ColorSquare.vue";
-import UpBar from "./components/UpBar.vue";
-</script>
+
 
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen">
+
+    <div class="flex flex-col justify-center w-screen min-h-screen">
       <UpBar/>
 
-      <div class="flex flex-row items-center justify-center gap-x-32">
-        <ColorSquare/>
-        <ColorSquare/>
-        <ColorSquare/>
+
+      <div class="w-full flex flex-row gap-x-32 justify-center"> 
+
+        <ColorSquare :color="colorA" />
+        <ColorSquare :color="resultingColor"/>
+        <ColorSquare :color="colorB"/>
       </div>
-      
     </div>
 
 </template>
+
+
+<script setup>
+import ColorSquare from "./components/ColorSquare.vue";
+import UpBar from "./components/UpBar.vue";
+import { ref } from "vue";
+
+const colorA = ref("#ff5733");
+const colorB = ref("#f22753");
+const resultingColor = ref( combineColors(colorA, colorB));
+
+
+function hexToRgb(hex) {
+    // Remove the hash (#) if it's there
+    hex = hex.substring(1, 6);
+
+    let bigint = parseInt(hex, 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+
+    return [r, g, b];
+}
+
+function combineColors(hex1, hex2) {
+    const rgb1 = hexToRgb(hex1.value);
+    const rgb2 = hexToRgb(hex2.value);
+    
+    // Average the RGB values
+    const r = Math.round((rgb1[0] + rgb2[0]) / 2);
+    const g = Math.round((rgb1[1] + rgb2[1]) / 2);
+    const b = Math.round((rgb1[2] + rgb2[2]) / 2);
+    
+    return rgbToHex(r, g, b);
+}
+
+function rgbToHex(r, g, b) {
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
+}
+
+</script>
 
 <style scoped>
 header {
--- colorrage/src/components/ColorSquare.vue
+++ colorrage/src/components/ColorSquare.vue
@@ -1,16 +1,24 @@
 <template>
-
-    <span :class="props.color" class="w-40 h-40 rounded shadow-sm m-0 p-0">
-    </span>
-
+    <div>
+        <div :style="colorClass" class="w-40 h-40 rounded shadow-sm m-0 p-0">
+        </div>
+        <p>{{ props.color }}</p>
+    </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 
-    const props = defineProps({
-        color: {
-            type: String,
-            default: 'bg-red-500'
-        }
-    });
+const props = defineProps({
+    color: {
+        type: String,
+        default: '#06b6d4'
+    }
+});
+
+const colorClass = computed(() => {
+    return {
+        backgroundColor: props.color
+    };
+});
 </script>
