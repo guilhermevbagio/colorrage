33a86b5800e82326a99a2c6ce18e1ed01818d5f2
Guilherme <guilhermevbagio@gmail.com>
2025-01-16 19:51:22 -0300

    fix resoluçao esquisita

--- colorrage/src/App.vue
+++ colorrage/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row items-center justify-center min-w-screen min-h-screen gap-y-8 bg-black overflow-hidden">
+  <div class="flex flex-col items-center justify-center w-screen min-w-screen min-h-screen gap-y-8 bg-black overflow-hidden">
       <UpBar />
 
       <ScreenCarousel :screens="screens" />
